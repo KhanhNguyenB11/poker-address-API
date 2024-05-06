a1d928540be1a5bef7c0962db3231b2862b8eb40
KhanhNguyenB11 <dantedmc1134@gmail.com>
2024-05-06 18:55:23 +0700

    deploy

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 app.use(express.json());
-require("dotenv").config();
 const mongoose = require("mongoose");
 app.use(cors({
   origin: "*"
